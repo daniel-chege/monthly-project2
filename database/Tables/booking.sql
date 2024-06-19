9287d4689788f072c9bfb161e1bed499c581a675
daniel-chege <126775594+daniel-chege@users.noreply.github.com>
2024-06-19 12:51:50 +0300

    Update booking.sql

--- database/Tables/booking.sql
+++ database/Tables/booking.sql
@@ -3,6 +3,6 @@ USE backend;
 create table booking(
 bookingId VARCHAR(255) PRIMARY KEY,
 userName VARCHAR(255) NOT NULL,
-bookPrice INT NOT NULL,
+price INT NOT NULL,
 userEmail VARCHAR(255) NOT NULL
 );
